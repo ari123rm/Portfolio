62b74b1d3c91e82220432d2f976d1cc8f7ea46a9
ari123rm <arielmoraes05@gmail.com>
2024-10-19 12:31:14 -0300

    theme: adicionar fundo e tamanho para capa

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import capaStyle from './components/themes/capa.module.scss'
 
 function App() {
   const conteudo:Array<Temas> = [];
-  conteudo.push(new Temas("Olá, sou o Ariel Rocha de Moraes",capaStyle.capa, logoImg, "Engenharia de Computação do IFCE - Campus Fortaleza"));
+  conteudo.push(new Temas("Olá, sou o <span>Ariel Rocha de Moraes</span>",capaStyle.capa, logoImg, "Engenharia de Computação do IFCE - Campus Fortaleza"));
 
   console.log(conteudo);
   
--- src/components/Topico.tsx
+++ src/components/Topico.tsx
@@ -1,15 +1,18 @@
 import { ReactElement,useState } from "react";
-
+import Parser from 'html-react-parser';
 import {Temas} from "./types/classes";
 
 
 const Topico:React.FC<Temas> = ({titulo,style,imagem,subtitulo,texto,lista})  => {
     return (
         <div className={style}>
-            <h1>{titulo}</h1>
-            {subtitulo!=undefined ? (
-                <h2>{subtitulo}</h2>
-                ):(<></>)}
+            <div>
+                <h1>{Parser(titulo)}</h1>
+                {subtitulo!=undefined ? (
+                    <h2>{subtitulo}</h2>
+                    ):(<></>)}
+            </div>
+            
             {(imagem != undefined) ? (
                 <img src={imagem.url} alt={imagem.alt} />
             ):(
