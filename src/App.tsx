e195e72d26df57609e1e7d3dd1763b8136b5ff96
ari123rm <arielmoraes05@gmail.com>
2024-10-20 02:16:20 -0300

    fix:posicionar tecnologias no lugar correto

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import Temas from './components/Temas';
 import Item from './components/Item';
 import { logoImg,medalhaImg } from './components/imagens';
 import style from './components/themes/topicos.module.scss'
-import { FaGithub } from "react-icons/fa";
+import { FaGithub,FaGitAlt,FaHtml5,FaCss3,FaJs,FaReact,FaNodeJs,FaCuttlefish ,FaJava } from "react-icons/fa";
+import { SiTypescript,SiHandlebarsdotjs,SiDart,SiFlutter,SiExpress } from "react-icons/si";
 function App() {
-  const tecnologias:Array<Item> = [new Item({tema:"Git"}),new Item({tema:"Github",icon:<FaGithub/> }),new Item({tema:"HTML"}),new Item({tema:"CSS"}),new Item({tema:"JavaScript"}),new Item({tema:"React"}),new Item({tema:"TypeScript"}),new Item({tema:"Node.js"}),new Item({tema:"HandleBars"}),new Item({tema:"C"}),new Item({tema:"JAVA"}),new Item({tema:"Dart"}),new Item({tema:"Flutter"})]
+  const tecnologias:Array<Item> = [new Item({tema:"Git",icon:<FaGitAlt/>}),new Item({tema:"Github",icon:<FaGithub/> }),new Item({tema:"HTML",icon:<FaHtml5/>}),new Item({tema:"CSS",icon:<FaCss3/>}),new Item({tema:"JavaScript",icon:<FaJs/>}),new Item({tema:"React",icon:<FaReact/>}),new Item({tema:"TypeScript",icon:<SiTypescript/>}),new Item({tema:"Node.js",icon:<FaNodeJs />}),new Item({tema:"Express",icon:<SiExpress/>}),new Item({tema:"HandleBars",icon:<SiHandlebarsdotjs/>}),new Item({tema:"C",icon:<FaCuttlefish />}),new Item({tema:"JAVA",icon:<FaJava/>}),new Item({tema:"Dart",icon:<SiDart/>}),new Item({tema:"Flutter",icon:<SiFlutter/>})]
   const conteudo:Array<Temas> = [];
  
   conteudo.push(new Temas({style: style.capa,titulo:"Olá, sou <br><span>Ariel Rocha de Moraes</span>", imagem:logoImg, subtitulo:"Estudante de Engenharia de Computação <br> IFCE - Campus Fortaleza"}));
