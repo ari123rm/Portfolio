6270a3411562b69fecfb00386d3a2c79dce42a61
ari123rm <arielmoraes05@gmail.com>
2024-10-20 21:40:04 -0300

    fix: separar conteudo de Item em ItemContent

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,44 +1,17 @@
 import React from "react";
-import Parser from 'html-react-parser';
 import itemProps from "./types/itemProps";
-
+import ItemContent from "./ItemContent";
 export default class Item extends React.Component<itemProps>{
-    itens = this.props.lista?.map((item)=>item.render())
+  
     render(){
         return(
             <li>
                 {this.props.href?(
                     <a href={this.props.href}>
-                        {this.props.icon  ? (<>{this.props.icon}</>):(<></>)}
-                    
-                        <div>
-                            <h3>{Parser(this.props.tema)}</h3>
-                            {this.props.texto  ? (<p>{Parser(this.props.texto)}</p>):(<></>)}
-                            {this.props.lista?(
-                            <ul>
-                                {this.itens}
-                            </ul>
-                        ):(<></>)}
-                        </div>
-                        {this.props.imagem ? ( <> {this.props.imagem.render()}</>):(<></>) }
-                        
+                        <ItemContent tema =  {this.props.tema} icon= {this.props.icon} lista={this.props.lista} texto = {this.props.texto} imagem={this.props.imagem} />
                     </a>
                 ):( 
-                    <>
-                        {this.props.icon  ? (<>{this.props.icon}</>):(<></>)}
-                        
-                        <div>
-                            <h3>{Parser(this.props.tema)}</h3>
-                            {this.props.texto  ? (<p>{Parser(this.props.texto)}</p>):(<></>)}
-                            {this.props.lista?(
-                                <ul>
-                                    {this.itens}
-                                </ul>
-                            ):(<></>)}
-                        </div>
-                        {this.props.imagem ? ( <> {this.props.imagem.render()}</>):(<></>) }
-                        
-                    </> 
+                    <ItemContent tema =  {this.props.tema} icon= {this.props.icon} lista={this.props.lista} texto = {this.props.texto} imagem={this.props.imagem} />
                 )}
                
             </li>
---
+++ src/components/ItemContent.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import itemProps from "./types/itemProps";
+import Parser from 'html-react-parser';
+export default class ItemContent extends React.Component<itemProps>{
+    itens = this.props.lista?.map((item)=>item.render())
+    render(){
+        return(
+            <>
+            {this.props.icon  ? (<>{this.props.icon}</>):(<></>)}         
+            <div>
+                <h3>{Parser(this.props.tema)}</h3>
+                {this.props.texto  ? (<p>{Parser(this.props.texto)}</p>):(<></>)}
+                {this.props.lista?(
+                <ul>
+                    {this.itens}
+                </ul>
+            ):(<></>)}
+            </div>
+            {this.props.imagem ? ( <> {this.props.imagem.render()}</>):(<></>) }
+            </>
+        )
+    }
+}
